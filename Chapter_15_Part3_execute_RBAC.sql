c01c9030e73a5875732bc36aa17e9944b95bccea
dominion21 <133910589+dominion21@users.noreply.github.com>
2024-12-31 10:21:46 -0500

    Update Chapter_15_Part3_execute_RBAC.sql

--- Chapter_15_Part3_execute_RBAC.sql
+++ Chapter_15_Part3_execute_RBAC.sql
@@ -10,8 +10,8 @@ execute immediate from @SF_DE/branches/main/Chapter_15/Chapter_15_Part2_RBAC.sql
 -- grant the functional roles to the users who perform those business functions
 -- in this exercise we grant both functional roles to ourself to be able to test them
 use role USERADMIN;
-grant role DATA_ENGINEER to user <your username>;
-grant role DATA_ANALYST to user <your username>;
+grant role DATA_ENGINEER to user dbt;
+grant role DATA_ANALYST to user dbt;
 
 -- switch to the DATA_ENGINEER role and verify that you can list files from the Git repository stage
 use role DATA_ENGINEER;
